23a633c6c61d3d07741243fa58aee71557bfd6ae
RaihanPrasetia <133187330+RaihanPrasetia@users.noreply.github.com>
2024-08-11 05:11:10 +0700

    old

--- database/connection.go
+++ database/connection.go
@@ -3,7 +3,6 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -11,13 +10,8 @@ import (
 var DB *sql.DB
 
 func Connect() {
-	// Ambil DSN dari variabel lingkungan
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
-		// Jika variabel lingkungan tidak diatur, gunakan default
-		dsn = "root@tcp(127.0.0.1:3306)/learn_db"
-	}
 
+	dsn := "root@tcp(127.0.0.1:3306)/learn_db"
 	var err error
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
